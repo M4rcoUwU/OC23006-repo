68c39db8bac70581cbeba0f89f2caebb043a7a2f
M4rcoUwU <oc23006@ues.edu.sv>
2025-05-11 18:10:54 -0600

    Add files via upload

---
+++ demo-oc23006/src/main/java/com/iis/oc23006/App.java
@@ -0,0 +1,14 @@
+package com.iis.oc23006;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public String Hello() {
+		return "Hello World";
+	}
+    public static void main(String[] args) {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
